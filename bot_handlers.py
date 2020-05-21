ab55f51c8ff7ed30ee910f242a25622b0320ac3e
Limbreaker <39244881+Limbreaker@users.noreply.github.com>
2020-05-21 22:00:37 +0300

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -32,7 +32,7 @@ def send_welcome(message):
                     markup.add(item1, item2)
                     list1 = []
     markup.add('Оставить заявку✉')
-    bot.send_message(message.chat.id, HELLO_MESSAGE + 'Для начала выберите категорию:', parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id, HELLO_MESSAGE + 'Для начала выберите модель:', parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(content_types=["text"])
@@ -65,64 +65,43 @@ def repeat_all_messages(message):
                         list1 = []
                 markup.add(list1[0], '/start')
                 markup.add('Оставить заявку✉')
-            bot.send_message(message.chat.id, 'Выберите модель: ', reply_markup=markup)
+            bot.send_message(message.chat.id, 'Выберите количество памяти: ', reply_markup=markup)
         for name2 in MESSAGES[name]:
             # Вторая проверка
             if message.text == name2:
                 for name3 in MESSAGES[name][name2]:
-                    count2 += 1
-                if count2 % 2 == 0:
-                    for name3 in MESSAGES[name][name2]:
-                        list2.append(name3)
-                        if len(list2) == 2:
-                            markup.add(list2[0], list2[1])
-                            list2 = []
-                    markup.add('/start', 'Оставить заявку✉')
-                else:
-                    for name3 in MESSAGES[name][name2]:
-                        list2.append(name3)
-                        if len(list2) == 2:
-                            markup.add(list2[0], list2[1])
-                            list2 = []
-                    markup.add(list2[0], '/start')
-                    markup.add('Оставить заявку✉')
-                bot.send_message(message.chat.id, 'Выберите количество памяти: ', reply_markup=markup)
+                    markup.add(name3)
+                markup.add('/start', 'Оставить заявку✉')
+                bot.send_message(message.chat.id, 'Выберите состояние, в котором пребывает Ваш телефон: ', reply_markup=markup)
+                for k,v in STATE.items():
+                    bot.send_message(message.chat.id, f'<b>{k}</b>: {v}\n', parse_mode='html')
             for name3 in MESSAGES[name][name2]:
-                        # Третья проверка
                 if message.text == name3:
-                    for name4 in MESSAGES[name][name2][name3]:
-                        markup.add(name4)
-                    markup.add('/start', 'Оставить заявку✉')
-                    bot.send_message(message.chat.id, 'Выберите состояние, в котором пребывает Ваш телефон: ', reply_markup=markup)
-                    for k,v in STATE.items():
-                        bot.send_message(message.chat.id, f'<b>{k}</b>: {v}\n', parse_mode='html')
-                for name4 in MESSAGES[name][name2][name3]:
-                    if message.text == name4:
-                        markup.add('Оставить заявку✉')
-                        markup.add('/start')
-                        if message.from_user.username is None:
-                            bot.send_message(message.chat.id, f'<b>Cпасибо, Ваше сообщение получено!\n</b>'
-                                                              f'<b>Детали:</b> {name4} \n'
-                                                              f'<b>Средняя стоимость:</b> {MESSAGES[name][name2][name3][name4]} \n'
+                    markup.add('Оставить заявку✉')
+                    markup.add('/start')
+                    if message.from_user.username is None:
+                        bot.send_message(message.chat.id, f'<b>Cпасибо, Ваше сообщение получено!\n</b>'
+                                                              f'<b>Детали:</b> {name3} \n'
+                                                              f'<b>Средняя стоимость:</b> {MESSAGES[name][name2][name3]} \n'
                                                               f'Но мы заметили что у Вас нету логина @Telegram\n'
                                                               f'Введите Ваш номер телефона: (В формате 380ХХХХХХХХХ)',
                                              parse_mode='html', reply_markup=markup)
-                            bot.send_message(725423821,
+                        bot.send_message(725423821,
                                              f'<b>{message.from_user.id} {message.from_user.username} отправил сообщение!</b> \n'
-                                             f'<b>Детали:</b> {name4} \n'
-                                             f'<b>Cтоимость:</b> {MESSAGES[name][name2][name3][name4]}',
+                                             f'<b>Детали:</b> {name3} \n'
+                                             f'<b>Cтоимость:</b> {MESSAGES[name][name2][name3]}',
                                              parse_mode='html')
-                        else:
-                            bot.send_message(message.chat.id, f'<b>Cпасибо, Ваше сообщение получено!\n</b>'
-                                                              f'<b>Детали:</b> {name4} \n'
-                                                              f'<b>Средняя стоимость:</b> {MESSAGES[name][name2][name3][name4]} \n'
+                    else:
+                        bot.send_message(message.chat.id, f'<b>Cпасибо, Ваше сообщение получено!\n</b>'
+                                                              f'<b>Детали:</b> {name3} \n'
+                                                              f'<b>Средняя стоимость:</b> {MESSAGES[name][name2][name3]} \n'
                                                               f'Для более детального обсуждения: 0935581747 или оставьте заявку и мы свяжемся с Вами в течении 30 минут😉',
                                              parse_mode='html', reply_markup=markup)
-                            bot.send_message(725423821, f'<b>{message.from_user.id} {message.from_user.username} отправил сообщение!</b> \n'
-                                                        f'<b>Детали:</b> {name4} \n'
-                                                        f'<b>Cтоимость: </b> {MESSAGES[name][name2][name3][name4]}' ,parse_mode='html')
+                        bot.send_message(725423821, f'<b>{message.from_user.id} {message.from_user.username} отправил сообщение!</b> \n'
+                                                        f'<b>Детали:</b> {name3} \n'
+                                                        f'<b>Cтоимость: </b> {MESSAGES[name][name2][name3]}' ,parse_mode='html')
 
-                        break
+                    break
 
     if message.text != '/start':
         list11 = []
@@ -135,8 +114,6 @@ def repeat_all_messages(message):
                 list12.append(name1)
                 for name2 in MESSAGES[name][name1]:
                     list13.append(name2)
-                    for name3 in MESSAGES[name][name1][name2]:
-                        list14.append(name3)
         if '380' in str(message.text) and len(str(message.text)) == 12:
             markup.add('/start')
             bot.send_message(message.chat.id, 'Ваш номер телефона принят. \n'
@@ -150,7 +127,7 @@ def repeat_all_messages(message):
                                               '(В формате 380ХХХХХХХХХ)',
                              parse_mode='html', reply_markup=markup)
 
-        elif message.text not in list11 and message.text not in list12 and message.text not in list13 and message.text not in list14:
+        elif message.text not in list11 and message.text not in list12 and message.text not in list13
             bot.send_message(message.chat.id, 'Я Вас не понял 😥 \n'
                                               'Нажмите на элемент клавиатуры либо введите сообщение /start для возврата в начало', parse_mode='html')
 
